63f5c4e3960b2e824b3e6cdf5eb92d88b102c595
MRamirez166 <MRamirezzz133@gmail.com>
2025-03-17 13:24:09 -0400

    3/17/2025

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,39 +1,38 @@
-public class main {
-    private static void quickSort(int[] array){
+public class main(String[] args){
+
+public class Main{
+    private static void quickSort(int[] array) {
         //Arguments
-        quickSort(array, 0, array.length -1);
+        quickSort(array, 0, array.length - 1);
     }
-    private static void quickSort(int[] array, int low, int high){
-        if(low<high){
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
             //Identifying values of the arrays to check weather or not a swap should be made.
             int changeIndex = partition(array, low, high);
-            quickSort(array, low, changeIndex-1);
-            quickSort(array, changeIndex+1, high);
+            quickSort(array, low, changeIndex - 1);
+            quickSort(array, changeIndex + 1, high);
         }
     }
+
     private static int partition(int[] array, int low, int high) {
         //variables
         int change = array[high];
-        int i = (low-1);
+        int i = (low - 1);
 
-        for(int j = low; j < high; j++) {
-            if(array[j] < change){
+        for (int j = low; j < high; j++) {
+            if (array[j] < change) {
                 i++;
                 //switch the position of value i and value j.
-                int i = temp;
-                swap(array,i, j);
-                return i +1;
-            }
-            private static void swap(int[] array, int i, int j){
                 int temp = array[i];
                 array[i] = array[j];
                 array[j] = temp;
             }
-            public static void main(String[] args){
-                int[] array = {3,1,9,3,4};
-                System.out.println("OG Array:");
-                System.out.print(array);
-            }
         }
+        int temp = array[i + 1];
+        array[i+1] = array[high];
+        array[high] = temp;
+        return i + 1;
     }
+}
 }
